230a3332855d0f980d71e3b472547e8ccdcb4529
--replace <david.thuku@studentmoringa.school.com>
2023-06-12 15:46:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const remove =document.getElementById("main");
+const toa=remove.remove()
+
+const newHeader =document.createElement("h1")
+
+newHeader.setAttribute('id','victory')
+newHeader.textContent="Kimemia is the champion";
+
+document.body.append(newHeader)
+
